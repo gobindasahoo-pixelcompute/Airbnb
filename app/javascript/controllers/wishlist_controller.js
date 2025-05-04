36643fd668af4d32698decce20f0691968fa9add
gobindasahoo-pixelcompute <gobinda.sahoo@pixelcompute.com>
2025-05-05 00:16:26 +0530

    feat: wishlist can be done only when user is loggedin

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -6,6 +6,11 @@ export default class extends Controller {
     console.log("wishlist controller is connected")
   }
   updateWishListStatus(){
+    const isUserLoggedIn = this.heartTarget.dataset.userLoggedIn;
+    if(isUserLoggedIn === "false"){
+      document.querySelector(".js-login").click();
+      return;
+    }
     console.log(this.heartTarget.dataset.status)
     this.heartTarget.dataset.status = (this.heartTarget.dataset.status === "false" ? "true" : "false")
     this.heartTarget.classList.toggle("fill-red-500")
